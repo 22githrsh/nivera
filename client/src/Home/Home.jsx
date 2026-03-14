a50c89a74a62b127f199f8d1511db5a288cab791
22githrsh <senh2675@gmail.com>
2026-03-14 21:51:53 +0530

    home updated

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -2,9 +2,9 @@ function Home(){
 
 return(
 
-<div>
+<div className="min-h-[100vh] w-full bg-[#fafafa] flex items-center justify-center">
 
-<h1>Welcome to Nivera</h1>
+<h1 className="text-[8rem]">Welcome to <span className="bg-orange-400">Nivera</span></h1>
 
 </div>
 
