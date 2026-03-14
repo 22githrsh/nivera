58d49d769b0a495f31e23d37fecd43e66e54170c
22githrsh <senh2675@gmail.com>
2026-03-14 20:15:24 +0530

    employer remove

--- client/src/routes/routesConfig.jsx
+++ client/src/routes/routesConfig.jsx
@@ -2,14 +2,12 @@ import { lazy } from "react";
 
 const Login = lazy(() => import("../pages/Auth/Login"));
 const Register = lazy(() => import("../pages/Auth/Register"));
-const EmployerLogin = lazy(() => import("../pages/Auth/EmployerLogin"));
 const VerifyOtp = lazy(() => import("../pages/Auth/VerifyOtp"));
 const Home = lazy(() => import("../Home/Home"));
 
 const routes = [
   { path: "/", element: <Login /> },
   { path: "/register", element: <Register /> },
-  { path: "/employer-login", element: <EmployerLogin /> },
   { path: "/verify-otp", element: <VerifyOtp /> },
   { path: "/home", element: <Home /> }
 ];
